215785ed9c796b14fdb4afbebf7cc293c3c3b965
c4324r <baconlord123456@gmail.com>
2023-01-24 10:03:05 -0800

    motor driver test2

--- src/motor_driver.py
+++ src/motor_driver.py
@@ -44,6 +44,6 @@ if __name__ == "__main__":
     en_pin = pyb.Pin(pyb.Pin.board.PA10, pyb.Pin.OUT_OD, pyb.Pin.PULL_UP)
     in1pin = pyb.Pin(pyb.Pin.board.PB4, pyb.Pin.OUT_PP)
     in2pin = pyb.Pin(pyb.Pin.board.PB4, pyb.Pin.OUT_PP)
-    tim = pyb.Timer(3, prescalar = 0 , preiod = 0xFFFF)
+    tim = pyb.Timer(3, prescaler = 0 , period = 0xFFFF)
     moe = MotorDriver(en_pin, in1pin, in2pin, tim)
     moe.set_duty_cycle(-42)
