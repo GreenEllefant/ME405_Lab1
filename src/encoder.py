2d527e420f683581a2c973a835a08084165cd412
GreenEllefant <germanjellsworth@gmail.com>
2023-01-24 11:11:53 -0800

    Does read. Can confirm.

--- src/encoder.py
+++ src/encoder.py
@@ -5,21 +5,29 @@ This file contains code definitions for encoder behavior.
 @date   24-Jan-2023
 @copyright (c) 2023 by Nobody and released under GNU Public License v3
 """
+import time
+
 def constructor_init():
-    pinB6= pyb.Pin(pyb.Pin.board.PB6, pyb.Pin.IN_PP)            #set pinB6 to input
-    pinB7= pyb.Pin(pyb.Pin.board.PB7, pyb.Pin.IN_PP)            #set pinB7 to input
-    pinC6= pyb.Pin(pyb.Pin.board.PC6, pyb.Pin.IN_PP)            #set pinC6 to input
-    pinC7= pyb.Pin(pyb.Pin.board.PC7, pyb.Pin.IN_PP)            #set pinC7 to input   
+    pinB6= pyb.Pin(pyb.Pin.board.PB6, pyb.Pin.IN)            #set pinB6 to input
+    pinB7= pyb.Pin(pyb.Pin.board.PB7, pyb.Pin.IN)            #set pinB7 to input
+    pinC6= pyb.Pin(pyb.Pin.board.PC6, pyb.Pin.IN)            #set pinC6 to input
+    pinC7= pyb.Pin(pyb.Pin.board.PC7, pyb.Pin.IN)            #set pinC7 to input   
     
-    tim4 = pyb.Timer(prescaler=0, period=0xFFFF)             #set Timer to max period
+    tim4 = pyb.Timer(4, prescaler=0, period=0xFFFF)             #set Timer to max period
     ch_1B = tim4.channel(1, pyb.Timer.ENC_AB, pin=pinB6) #export timer channel variable											      
     ch_2B = tim4.channel(2, pyb.Timer.ENC_AB, pin=pinB7) #export timer channel variable
 
-    tim3 = pyb.Timer(prescaler=0, period=0xFFFF)             #set Timer to max period
+
+
+    tim3 = pyb.Timer(3, prescaler=0, period=0xFFFF)             #set Timer to max period
     ch_1C = tim3.channel(1, pyb.Timer.ENC_AB, pin=pinC6) #export timer channel variable											      
     ch_2C = tim3.channel(2, pyb.Timer.ENC_AB, pin=pinC7) #export timer channel variable
 
-    return ch_1B
+    for i in range(50):
+        print(tim4.counter())
+        time.sleep(0.1)
+
+    
     
 
 
@@ -29,6 +37,4 @@ def method_read():
 def method_zero():
     pass
 
-ch_1B = constructor_init()
-out = ch_1B.counter()
-print(out)
+constructor_init()
