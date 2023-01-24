58212e4f009cc502f12b5629d10c15a57c6689e9
GreenEllefant <germanjellsworth@gmail.com>
2023-01-24 10:05:59 -0800

    typos

--- src/motor_driver.py
+++ src/motor_driver.py
@@ -16,8 +16,8 @@ class MotorDriver:
         self.ch1 = self.timer.channel(1, pyb.Timer.PWM, pin=self.in1pin)
         self.ch2 = self.timer.channel(2, pyb.Timer.PWM, pin=self.in2pin)
         self.en_pin.high()
-        ch1.pulse_width_percent(0)
-        ch2.pulse_width_percent(0)
+        self.ch1.pulse_width_percent(0)
+        self.ch2.pulse_width_percent(0)
         print ("Creating a motor driver")
 
     def set_duty_cycle (self, level):
@@ -30,7 +30,7 @@ class MotorDriver:
                cycle of the voltage sent to the motor 
         """
         if(level > 0):
-            self.ch1.pulse_width_percent(level)
+            self.ch1.pulse_width_percent(level*-1)
             self.ch2.pulse_width_percent(0)
         elif(level < 0):
             self.ch1.pulse_width_percent(0)
@@ -44,6 +44,6 @@ if __name__ == "__main__":
     en_pin = pyb.Pin(pyb.Pin.board.PA10, pyb.Pin.OUT_OD, pyb.Pin.PULL_UP)
     in1pin = pyb.Pin(pyb.Pin.board.PB4, pyb.Pin.OUT_PP)
     in2pin = pyb.Pin(pyb.Pin.board.PB4, pyb.Pin.OUT_PP)
-    tim = pyb.Timer(3, prescalar = 0 , preiod = 0xFFFF)
+    tim = pyb.Timer(3, prescaler = 0 , period = 0xFFFF)
     moe = MotorDriver(en_pin, in1pin, in2pin, tim)
-    moe.set_duty_cycle(-42)
+    moe.set_duty_cycle(100)
