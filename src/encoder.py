b3b82bb377b58d586afcbfd7b696ecbc3b4adea3
Hannah Howe <57115504+ElectroHannah@users.noreply.github.com>
2023-01-29 12:56:23 -0800

    Update encoder.py

--- src/encoder.py
+++ src/encoder.py
@@ -7,7 +7,7 @@ This file contains code definitions for encoder behavior.
 """
 import time
 
-def constructor_init():
+def enc_read():
     pinB6= pyb.Pin(pyb.Pin.board.PB6, pyb.Pin.IN)            #set pinB6 to input
     pinB7= pyb.Pin(pyb.Pin.board.PB7, pyb.Pin.IN)            #set pinB7 to input
     pinC6= pyb.Pin(pyb.Pin.board.PC6, pyb.Pin.IN)            #set pinC6 to input
@@ -28,13 +28,39 @@ def constructor_init():
         time.sleep(0.1)
 
     
-    
+class Encoder_Reader:
+    def __init__(self, en1_pin, en2_pin, timer4):
+        self.en1_pin = en1_pin
+        self.en2_pin = en2_pin
+        self.timer4 = timer4
+        self.reference_count = self.timer4.counter()
+        self.prev_pos = 0
+        self.timer4.callback(self.handleflow)
+        self.ch1B = self.timer4.channel(1, pyb.Timer.ENC_AB, pin=en1_pin)
+        self.ch2B = self.timer4.channel(2, pyb.Timer.ENC_AB, pin=en2_pin)
+        self.zero()
 
+    def read(self):
+        self.prev_pos = self.timer4.counter() - self.reference_count
+        return self.prev_pos
+            #print(current_count)
 
-def method_read():
-    pass
+    def zero(self):
+        self.reference_count = self.timer4.counter()
 
-def method_zero():
-    pass
+    def handleflow(self, tim):
+        current = self.timer4.counter()
+        if(current < self.prev_pos):
+            self.reference_count -= 0xFFFF
+        else:
+            self.reference_count += 0xFFFF
 
-constructor_init()
+if __name__ == "__main__":
+    en1_pin = pyb.Pin(pyb.Pin.board.PB6, pyb.Pin.IN)
+    en2_pin = pyb.Pin(pyb.Pin.board.PB7, pyb.Pin.IN)
+    timer4 = pyb.Timer(4, prescaler=0, period=0xFFFF) 
+    reader = Encoder_Reader(en1_pin, en2_pin, timer4)
+    for i in range(0, 50):
+        print(reader.read())
+        time.sleep(0.1)
+
